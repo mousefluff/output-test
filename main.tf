18a24a75c310ffe3d82a9f88114fa8f46d091d94
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-04-17 17:32:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 output "dotoken" {
-  value = "Terraform used the selected providers to generate the following execution \\x1b[31mThis text is red. <img src=x/><h1>html?</h1>"
+  value = "Terraform used the selected providers to generate the following execution \u001b[31mThis text is red. <img src=x/><h1>html?</h1>"
 }
